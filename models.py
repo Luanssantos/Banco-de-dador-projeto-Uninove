6370231135035a42255200c39485d766c3680120
Luanssantos <142276528+Luanssantos@users.noreply.github.com>
2025-10-05 17:24:45 +0000

    Projeto de estoque com SQLAlchemy

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -10,11 +12,8 @@ class Produto(Base):
     preco = Column(Float, nullable=False)
     estoque = Column(Integer, nullable=False)
 
-class Venda(Base):
-    __tablename__ = 'vendas'
-    id = Column(Integer, primary_key=True)
-    produto_id = Column(Integer, ForeignKey('produtos.id'), nullable=False)
-    quantidade = Column(Integer, nullable=False)
+    vendas = relationship("Venda", back_populates="produto")
+
 
 class Cadastro(Base):
     __tablename__ = 'cadastro'
@@ -22,3 +21,19 @@ class Cadastro(Base):
     nome = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     senha = Column(String, nullable=False)
+
+    vendas = relationship("Venda", back_populates="cliente")
+
+
+class Venda(Base):
+    __tablename__ = 'vendas'
+    id = Column(Integer, primary_key=True)
+    produto_id = Column(Integer, ForeignKey('produtos.id'), nullable=False)
+    cliente_id = Column(Integer, ForeignKey('cadastro.id'), nullable=True)
+    quantidade = Column(Integer, nullable=False)
+    valor_total = Column(Float, nullable=False)
+    data_venda = Column(DateTime, default=datetime.utcnow, nullable=False)
+    status = Column(String, default="concluída", nullable=False)
+
+    produto = relationship("Produto", back_populates="vendas")
+    cliente = relationship("Cadastro", back_populates="vendas")
